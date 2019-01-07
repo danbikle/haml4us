c35668cb5bf01ff0318bc50a3f028168ab9ce182
Ron Railsmith <railsmith4@outlook.com>
2019-01-07 00:57:03 -0800

    more

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'haml4us/demo1'
+  get 'haml4us/demo2'
+  get 'haml4us/demo3'
+  get 'haml4us/demo4'
+  root 'haml4us#demo1'
+  # For details on the DSL available within this file,
+  # see http://guides.rubyonrails.org/routing.html
 end
